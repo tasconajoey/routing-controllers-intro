dbd831b43cb6b020d4930183f228f8422ebae461
tasconajoey <joey.tascona@gmail.com>
2017-01-30 13:36:09 -0500

    Finish full assignment

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,26 @@
 class PagesController < ApplicationController
+  before_action :set_kitten_url, only: [:kitten, :kittens]
   def welcome
-    render :about
+    @header = "This is the welcome page"
+  end
+  def about
+  end
+  def contest
+    flash[:notice] = "Sorry, the contest has ended"
+    redirect_to "/welcome"
+  end
+  def kitten
+  end
+  def kittens
+  end
+  def set_kitten_url
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/cats"
+  end
+  def secrets
+    unless params[:magic_word] == "hello"
+      flash[:alert] = "Sorry, you're not authorized to see that page!"
+      redirect_to "/welcome"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get '/' => 'pages#welcome'
+  root 'pages#welcome'
   get '/welcome' => 'pages#welcome'
+  get '/contest' => 'pages#contest'
+  get '/kitten/:size' => 'pages#kitten', as: 'kitten'
+  get '/secrets/:magic_word' => 'pages#secrets'
 end
